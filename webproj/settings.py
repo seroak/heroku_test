7f9d37a61230b585c899cd8cfe363648e264ecf8
lee gue yeol <i1004gy@gmail.com>
2022-10-22 16:36:43 +0900

    deploy

--- webproj/settings.py
+++ webproj/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
